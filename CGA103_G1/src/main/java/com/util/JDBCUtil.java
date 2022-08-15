512c578c013bbaa7050c275593cf7ef3cb843593
chgv <chgv0926@gmail.com>
2022-08-15 22:28:21 +0800

    555

---
+++ CGA103_G1/src/main/java/com/util/JDBCUtil.java
@@ -0,0 +1,8 @@
+package com.util;
+
+public class JDBCUtil {
+	public static final String URL = "jdbc:mysql://localhost:3306/cga103g1?serverTimezone=Asia/Taipei";
+	public static final String USERNAME = "root";
+	public static final String PASSWORD = "tp61i6jo3";
+	public static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+}
